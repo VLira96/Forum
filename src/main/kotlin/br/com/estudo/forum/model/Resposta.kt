f27296cc54a5af495e141297a4e19ecc72988f19
VLira96 <lira94634@gmail.com>
2025-01-16 13:24:41 -0300

    Primeiro commit

---
+++ src/main/kotlin/br/com/estudo/forum/model/Resposta.kt
@@ -0,0 +1,13 @@
+package br.com.estudo.forum.model
+
+import java.time.LocalDateTime
+
+class Resposta(
+    val id: Long?,
+    val mensagem: String,
+    val dataCriacao: LocalDateTime = LocalDateTime.now(),
+    val autor: Usuario,
+    val topico: Topico,
+    val solucao: Boolean
+
+)
