cea1ece535a04ca5b5d140524b0d10284e9a6f68
VLira96 <lira94634@gmail.com>
2025-01-17 17:30:18 -0300

    ultimo commit antes de utilizar o Postman

--- src/main/kotlin/br/com/estudo/forum/controller/TopicoController.kt
+++ src/main/kotlin/br/com/estudo/forum/controller/TopicoController.kt
@@ -6,6 +6,8 @@ import br.com.estudo.forum.model.Usuario
 import br.com.estudo.forum.services.TopicoService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
@@ -21,5 +23,10 @@ class TopicoController(private val service: TopicoService) {
     @GetMapping("/{id}")
     fun buscarPorId(@PathVariable id: Long): Topico {
         return service.buscarPorId(id)
+    }
+
+    @PostMapping
+    fun cadastrar(@RequestBody topico: Topico) {
+        service.cadastrar(topico)
     }
 }
--- src/main/kotlin/br/com/estudo/forum/services/TopicoService.kt
+++ src/main/kotlin/br/com/estudo/forum/services/TopicoService.kt
@@ -7,56 +7,7 @@ import org.springframework.stereotype.Service
 import java.util.*
 
 @Service
-class TopicoService(private var topicos: List<Topico>) {
-
-    init {
-        val topico = Topico(
-            id = 1,
-            titulo = "Duvidas Kotlin",
-            mensagem = "Variáveis no Kotlin",
-            curso = Curso(
-                id = 1,
-                nome = "Kotlin",
-                categoria = "Programação",
-            ),
-            autor = Usuario(
-                id = 1,
-                nome = "Ana da Silva",
-                email = "teste@teste.com"
-            )
-        )
-        val topico2 = Topico(
-            id = 2,
-            titulo = "Duvidas Kotlin 2",
-            mensagem = "Variáveis no Kotlin 2",
-            curso = Curso(
-                id = 1,
-                nome = "Kotlin",
-                categoria = "Programação",
-            ),
-            autor = Usuario(
-                id = 1,
-                nome = "Ana da Silva",
-                email = "teste@teste.com"
-            )
-        )
-        val topico3 = Topico(
-            id = 3,
-            titulo = "Duvidas Kotlin 3",
-            mensagem = "Variáveis no Kotlin 3",
-            curso = Curso(
-                id = 1,
-                nome = "Kotlin",
-                categoria = "Programação",
-            ),
-            autor = Usuario(
-                id = 1,
-                nome = "Ana da Silva",
-                email = "teste@teste.com"
-            )
-        )
-        topicos = Arrays.asList(topico, topico2, topico3)
-    }
+class TopicoService(private var topicos: List<Topico> = ArrayList()) {
 
     fun listar(): List<Topico> {
         return topicos
@@ -66,5 +17,9 @@ class TopicoService(private var topicos: List<Topico>) {
         return topicos.stream().filter({
             t -> t.id == id
         }).findFirst().get()
+    }
+
+    fun cadastrar(topico: Topico) {
+        topicos.plus(topico)
     }
 }
